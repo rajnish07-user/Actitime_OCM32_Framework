9d0070477211e9d209f072a3dd854502251d0d1d
RAJNISH <RAJNISH@RKsingh>
2026-02-01 02:19:06 +0530

    commit it

--- myproject/src/Training/Day2.java
+++ myproject/src/Training/Day2.java
@@ -1,5 +1,17 @@
 package Training;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Day2 {
 
+	public void testbrowser()
+	{
+		
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	System.out.println("hello");
+	driver.quit();
+
+	}
 }
